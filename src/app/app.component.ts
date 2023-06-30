b757df6b218c23939d7c676033e6ab479d3aa0ab
Guillermo Riaño <guilledark17@gmail.com>
2023-06-30 09:51:31 -0600

    ✔️ Employee Array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular_CRUD_Simple_1';
+  title = 'Angular CRUD';
+
+  employeeArray: Employee[] =[
+    {id: 1, name: "Jade", country: "MX"},
+    {id: 2, name: "Peny", country: "MX"},
+    {id: 3, name: "Carl", country: "MX"},
+  ];
+
+  
 }
